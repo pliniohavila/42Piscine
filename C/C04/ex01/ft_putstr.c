7a44267fd72154def691fe2f523b0437dea61044
pliniocode <pliniohrcode@gmail.com>
2023-05-06 16:54:04 -0300

    ft_putstrc. done

---
+++ C/C04/ex01/ft_putstr.c
@@ -0,0 +1,22 @@
+#include "unistd.h"
+
+void     ft_putstr(char *str)
+{
+    int     i;
+
+    i = 0;
+    while(str[i] != '\0')
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+}
+
+int     main(void)
+{
+    char    *str = "Arto Tuncboyaciyan";
+
+    ft_putstr(str);
+
+    return (0);
+}
