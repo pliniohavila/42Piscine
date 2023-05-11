1ebd588c4a65f47716fc78563595bddeaeb71186
pliniohrcode <pliniohrcode@gmail.com>
2023-05-11 08:19:14 -0300

    ft_iterative_power changes

--- C/C05/ex02/ft_iterative_power.c
+++ C/C05/ex02/ft_iterative_power.c
@@ -7,12 +7,14 @@ int     ft_iterative_power(int nb, int power)
     int     r;
 
     r = nb;
-    if (nb < 0)
+    if ((nb == 0 && power != 0) || power < 0)
         return (0);
+    if ((powre == 0) || (nb < 0) && (power == 0))
+        return (1);
 
     for (int i = 1; (i < power) && (r > INT_MIN && r < INT_MAX); i++)
         r = r * nb;
-        
+
     return r; 
 }
 
