af2c47aebe5caf429bf370a05b26e006fd7e3ba2
pliniohrcode <pliniohrcode@gmail.com>
2023-05-11 08:11:47 -0300

    ft_iterative_power done

---
+++ C/C05/ex02/ft_iterative_power.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "limits.h"
+
+int     ft_iterative_power(int nb, int power)
+{
+    int     r;
+
+    r = nb;
+    if (nb < 0)
+        return (0);
+
+    for (int i = 1; (i < power) && (r > INT_MIN && r < INT_MAX); i++)
+        r = r * nb;
+        
+    return r; 
+}
+
+int     main(int argc, char *argv[])
+{
+    int     base;
+    int     exp;
+    int     ft;
+
+    if (argc != 3)
+        return (-1);
+
+    base = atoi(argv[1]);
+    exp = atoi(argv[2]);
+    ft = ft_iterative_power(base, exp);
+
+    printf("%d ** %d is %d\n", base, exp, ft); 
+
+    return (0);
+}
