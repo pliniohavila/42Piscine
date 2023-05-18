7cb92bebe5847710691b2a6d6973c58ed91efebf
pliniohrcode <pliniohrcode@gmail.com>
2023-05-18 08:31:13 -0300

    ft_print_params done

---
+++ C/C06/ex01/ft_print_params.c
@@ -0,0 +1,33 @@
+#include "unistd.h"
+
+void     ft_putstr(char *str)
+{
+    int     i;
+
+    i = 0;
+    while(str[i] != '\0')
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+}
+
+int     main(int argc, char **argv)
+{
+    if (argc < 1)
+        return (-1);
+
+    int     i;
+
+    i = 1;
+    while (i <= argc)
+    {
+        ft_putstr(argv[i]);
+        write(1, "\n", 1);
+        i++;
+    }
+
+    write(1, "\n", 1);
+
+    return (0);
+}
