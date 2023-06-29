84345e44ac5386b052dbdc4e3b4b1f8f82509cb5
pliniohrcode <pliniohrcode@gmail.com>
2023-06-29 12:44:37 -0300

    Makefile done

---
+++ C/C09/ex01/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "includes/libft.h"
+
+
+int     main(void)
+{
+    int     a;
+    int     b;
+    char    *str;
+
+    a = 42;
+    b = 21;
+    str = "Ecole42"; 
+    printf("SWAP(%d, %d): ", a, b);
+    ft_swap(&a, &b);
+    printf("%d, %d\n", a, b);
+    ft_putstr("PUTSTR: ");
+    ft_putstr(str);
+    printf("\n");
+    printf("LEN(%s): %d\n", str, ft_strlen(str));
+    printf("STRCMP(%s, hello): %d", str, ft_strcmp(str, "hellos"));
+    printf("\n");
+
+    return (0);
+}
