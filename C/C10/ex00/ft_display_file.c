514e009ad37ca50ada094f0eab462f6788fcb4ea
pliniocode <pliniohrcode@gmail.com>
2023-08-12 10:14:18 -0300

    Make ft_display_file

--- C/C10/ex00/ft_display_file.c
+++ C/C10/ex00/ft_display_file.c
@@ -1,8 +1,11 @@
-#include "includes/libft.h"
 #include <unistd.h>
-#include <stdio.h>
+#include <fcntl.h>
+#include "includes/libft.h"
+
+#define BUF_SIZE 1024
 
 void    ft_display_file(char *);
+void    clear_buf(char *);
 
 int     main(int argc, char **argv)
 {
@@ -23,9 +26,47 @@ int     main(int argc, char **argv)
 
 void    ft_display_file(char *file)
 {
-    // int     fd;
-    // char    buf[1024];
+    int     fd;
+    int     bytes_reads;
+    int     bytes_writes;
+    char    buf[1024];
 
-    // fd = open(file, O_RDONLY)
-    printf("FILE: %s\n", file);
+    fd = 0;
+    fd = open(file, O_RDONLY);
+    if (fd == -1)
+    {
+        write(2, "Error opening the file\n", 30);
+        return;
+    }
+
+    bytes_reads = 0;
+    bytes_writes = 0;
+    clear_buf(buf);
+    while((bytes_reads = read(fd, buf, BUF_SIZE)) > 0)
+    {
+        bytes_writes = write(1, buf, bytes_reads);
+        if (bytes_writes != bytes_reads)
+        {
+            write(2, "Error reading the file\n", 30);
+            close(fd);
+            return;
+        }
+    }
+
+    if (bytes_reads == -1)
+    {
+        write(2, "Error reading the file\n", 30);
+        close(fd);
+        return;
+    }
+
+    write(1, "\n\n", 2);    
+    close(fd);    
+}
+
+void    clear_buf(char *buf)
+{
+    int     i;
+    while(i < BUF_SIZE)
+        buf[i++] = '\0';
 }
