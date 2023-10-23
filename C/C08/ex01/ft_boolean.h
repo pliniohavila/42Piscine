b93d3e0fd70cba195bddc18f916e85fd34ff801f
pliniocode <pliniohrcode@gmail.com>
2023-10-23 19:25:12 -0300

    Make ft_boolean

--- C/C08/ex01/ft_boolean.h
+++ C/C08/ex01/ft_boolean.h
@@ -1,4 +1,9 @@
-
 typedef enum { TRUE, FALSE } t_bool;
 
-#define EVEN(nbr) (nbr % 2)
+#define EVEN(nbr) (((nbr) % (2)) == 0)
+
+#define EVEN_MSG "I have an even number of arguments."
+
+#define ODD_MSG "I have an odd number of arguments."
+
+#define SUCCESS 0
--- C/C08/ex01/main.c
+++ C/C08/ex01/main.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "ft_boolean.h"
 
 void    ft_putstr(char *str)
