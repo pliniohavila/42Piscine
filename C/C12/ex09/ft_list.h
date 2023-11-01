ea2103b2e13b67ccad056411405325b8a95a8ea4
pliniocode <pliniohrcode@gmail.com>
2023-11-01 17:27:59 -0300

    Make ft_list_foreach

--- C/C12/ex09/ft_list.h
+++ C/C12/ex09/ft_list.h
@@ -9,6 +9,8 @@ typedef struct          s_list
 
 t_list      *ft_create_elem(void *data);
 void        ft_list_push_front(t_list **begin_list, void *data);
-void        print_elem_data(void*);
+void        ft_list_foreach(t_list *begin_list, void (*f)(void *));
+void        print_elem_data(void *data);
+void        field_list(t_list **begin_list);
 
 #endif
--- C/C12/ex09/ft_list_foreach.c
+++ C/C12/ex09/ft_list_foreach.c
@@ -5,30 +5,24 @@
 
 t_list      *ft_create_elem(void*);
 void        ft_list_push_front(t_list**, void*);
+void        ft_list_foreach(t_list *begin_list, void (*f)(void *));
 void        print_elem_data(void*);
+void        field_list(t_list **begin_list);
 
 int     main(void)
 {
     int     nb;
-    int     nb2;
     t_list  *list;
 
     nb = 42;
-    nb2 = 21;
     list = ft_create_elem(&nb);
     if (list == NULL)
         return (1);
-    ft_list_push_front(&list, &nb2);
-    print_elem_data(list->data);
-    print_elem_data(list->next->data);
-
+    field_list(&list);
+    ft_list_foreach(list, print_elem_data);
     return(0);
 }
 
-void        print_elem_data(void *data)
-{
-    printf("Data: %d\n", *(int*)data);
-}
 
 void        ft_list_push_front(t_list **begin_list, void *data)
 {
@@ -51,4 +45,36 @@ t_list      *ft_create_elem(void *data)
     elem->next = NULL;
     elem->data = data;
     return (elem);
+}
+
+void        ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+    t_list      *list_ptr;
+
+    list_ptr  = begin_list;
+    while (list_ptr != NULL)
+    {
+        f(list_ptr->data);
+        list_ptr = list_ptr->next;
+    }
+}
+
+void        print_elem_data(void *data)
+{
+    printf("Data: %d\n", *(int*)data);
+}
+
+void        field_list(t_list **begin_list)
+{
+    int     data[5];
+    int     i;
+
+    data[0] = 56;
+    data[1] = 21;
+    data[2] = 3;
+    data[3] = 22;
+    data[4] = 9;
+    i = 0;
+    while (i < 5)
+        ft_list_push_front(begin_list, &data[i++]);
 }
